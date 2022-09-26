7fe9dada2f1c837012c5c4937d078e73d2dfe398
JiguangPeng <pengjiguang@gmail.com>
2022-09-26 18:58:29 +0800

    Odds Ratio Cal

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# odds-ratio
-Odds Ratio calculator
+# odds_ratio
+
+Odds Ratio Calculator
+
+a — positive (bad)  cases in exposed group
+b - negative (good) cases in exposed group
+c - positive (bad)  cases in control group
+d - negative (good) cases in control group
+
+Haldane-Anscombe correction for zero：add 0.5 for a/b/c/d.
+
+
+## Installation
+
+1. `pip install scipy`
+2. `git clone https://github.com/JiguangPeng/odds_ratio`
+
+
+## Usage
+
+
+```python
+from odds_ratio import OddsRatio
+
+test = OddsRatio(18, 2, 10, 10)
+
+test.odds_ratio          # 9.0
+test.odds_ratio_lower    # 1.6381043384832557
+test.odds_ratio_upper    # 49.447399715087194
+test.odds_ratio_ci       # '[1.64, 49.45]'
+
+
+test.pvalue_fisher       # 0.013814147851967653
+test.prob_fisher         # 0.006283257673691489
+```
---
+++ odds_ratio.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from math import log, exp, sqrt, factorial
+from scipy.stats import fisher_exact
+
+
+class OddsRatio():
+    """
+    Odds Ratio Calulator
+    a — positive (bad)  cases in exposed group
+    b - negative (good) cases in exposed group
+    c - positive (bad)  cases in control group
+    d - negative (good) cases in control group
+
+    Haldane-Anscombe correction for zero：add 0.5 for abcd
+    """
+
+    def __init__(self, a, b, c, d):
+        if int(a) * int(b) * int(c) * int(d) == 0:
+            self.a = int(a) + 0.5
+            self.b = int(b) + 0.5
+            self.c = int(c) + 0.5
+            self.d = int(d) + 0.5
+        else:
+            self.a = int(a)
+            self.b = int(b)
+            self.c = int(c)
+            self.d = int(d)
+        self.odds_ratio = (self.a/self.b) / (self.c/self.d)
+        self.standerr = sqrt(1/self.a + 1/self.b + 1/self.c + 1/self.d)
+        self.odds_ratio_upper = exp(log(self.odds_ratio) + 1.96 * self.standerr)
+        self.odds_ratio_lower = exp(log(self.odds_ratio) - 1.96 * self.standerr)
+
+    
+    @property
+    def odds_ratio_ci(self):
+        if self.odds_ratio >= 1:
+            upper = '{:.2f}'.format(self.odds_ratio_upper)
+            lower = '{:.2f}'.format(self.odds_ratio_lower)
+        else:
+            upper = '{:.3g}'.format(self.odds_ratio_upper)
+            lower = '{:.3g}'.format(self.odds_ratio_lower)
+            
+        return f'[{lower}, {upper}]'
+
+        
+    @property
+    def or_ci_qc(self):
+        if float(self.odds_ratio_lower) <= 1 <= float(self.odds_ratio_upper):
+            return 'FAIL'
+        else:
+            return 'PASS'
+
+    @property
+    def pvalue_fisher(self):
+        pvalue = fisher_exact([[self.a,self.b], [self.c,self.d]])[1]
+        return pvalue
+
+
+    @property
+    def pvalue_fisher_text(self):
+        return '{:.3g}'.format(pvalue)
+
+
+    @property
+    def prob_fisher(self):
+        a, b, c, d = map(int, (self.a, self.b, self.c, self.d))
+        m = factorial(a+b) * factorial(c+d) * factorial(a+c)  * factorial(b+d)
+        n = factorial(a) * factorial(b) * factorial(c) * factorial(d) * factorial(a+b+c+d)
+        return m/n
+
